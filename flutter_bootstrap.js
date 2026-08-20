72e6a97e55f4e152b012b1e5e99015615e372e82
saidiminda <isiakasaidi02@gmail.com>
2026-08-20 10:19:16 +0300

    Small changes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3055698475" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2802954168" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
